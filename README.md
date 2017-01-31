d2661bdc12a14ec2df614cef720831a71947bcf3
murphypicard <ayaghsizian@gmail.com>
2017-01-30 20:34:49 -0500

    form is structured need to add routes still

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
 This is a lesson that I am doing for my LaunchCode class.
 
+User must be able to enter a username and password
+
+
+
 ```py
 name = "Ara"
 ```
--- main.py
+++ main.py
@@ -16,9 +16,34 @@
 #
 import webapp2
 
+intro = "<h1>Ara's Signup Project for LaunchCode 101</h1>\n"
+form = """
+        <form action="/" method="POST">
+            <table>
+                <tr>
+                    <td>Username</td>
+                    <td><input name='username' type='text'/></td>
+                </tr>
+                <tr>
+                    <td>Password</td>
+                    <td><input name='password' type='text'/></td>
+                </tr>
+                <tr>
+                    <td>Verify Password</td>
+                    <td><input name='password' type='text'/></td>
+                </tr>
+                <tr>
+                    <td>Email (Optional)</td>
+                    <td><input name='email' type='text'/></td>
+                </tr>
+            </table>
+            <input type="submit">
+        </form>
+"""
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.response.write(intro + form)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
